d1de25382c0cffd19c481a26369857519982bfed
deepak smartsense <deepak.kumar@smartsensesolutions.com>
2024-11-05 10:12:56 +0530

    feat: added basic SEO support

--- app/layout.js
+++ app/layout.js
@@ -21,6 +21,65 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <>
+          <title>Nikita Sindhi - Business Development Executive</title>
+          <meta
+            name="title"
+            content="Nikita Sindhi - Business Development Executive"
+          />
+          <meta
+            name="description"
+            content="Discover the professional portfolio of Nikita Sindhi, a results-driven Business Development Executive with expertise in client acquisition, lead generation, and strategic sales growth."
+          />
+
+          <meta
+            name="keywords"
+            content="Nikita Sindhi, Business Development Executive, IT Sales, Client Acquisition, Lead Generation, Sales Strategy, Market Analysis, Ahmedabad, India"
+          />
+
+          <meta property="og:type" content="website" />
+          <meta
+            property="og:url"
+            content="https://nikita-sindhi-portfolio.vercel.app/"
+          />
+          <meta
+            property="og:title"
+            content="Nikita Sindhi - Business Development Executive"
+          />
+          <meta
+            property="og:description"
+            content="Explore Nikita Sindhi's portfolio - a business development executive with a track record of boosting client engagement and revenue growth."
+          />
+          <meta
+            property="og:image"
+            content="https://nikita-sindhi-portfolio.vercel.app/preview-image.jpg"
+          />
+
+          <meta property="twitter:card" content="summary_large_image" />
+          <meta
+            property="twitter:url"
+            content="https://nikita-sindhi-portfolio.vercel.app/"
+          />
+          <meta
+            property="twitter:title"
+            content="Nikita Sindhi - Business Development Executive"
+          />
+          <meta
+            property="twitter:description"
+            content="Discover the professional achievements and expertise of Nikita Sindhi in client acquisition, sales strategies, and market analysis."
+          />
+          <meta
+            property="twitter:image"
+            content="https://nikita-sindhi-portfolio.vercel.app/preview-image.jpg"
+          />
+
+          <link
+            rel="canonical"
+            href="https://nikita-sindhi-portfolio.vercel.app/"
+          />
+        </>
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
